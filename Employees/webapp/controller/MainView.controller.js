dd1fd503dff16d881d91983b69446fc0aef791c7
lualtabs <lualta@altasersas.com>
2021-04-18 19:07:50 +0000

    Second Commit 04

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -7,26 +7,23 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
-        return Controller.extend("logaligroup.Employees.controller.MainView", {
-            onInit: function () {
+        var Main = Controller.extend("logaligroup.Employees.controller.MainView", {});
 
-            },
-
-            onValidate: function () {
-                var inputEmployee = this.byId("inputEmployee");
+        Main.prototype.onValidate = function () {
+            var inputEmployee = this.byId("inputEmployee");
+            // @ts-ignore
+            var valueEmployee = inputEmployee.getValue();
+            if (valueEmployee.length === 6) {
                 // @ts-ignore
-                var valueEmployee = inputEmployee.getValue();
-                if (valueEmployee.length === 6) {
-                    // @ts-ignore
-                    //    inputEmployee.setDescription("OK");
-                    this.byId("labelCountry").setVisible(true);
-                    this.byId("slCountry").setVisible(true);
-                } else {
-                    // @ts-ignore
-                    //    inputEmployee.setDescription("Not OK");
-                    this.byId("labelCountry").setVisible(false);
-                    this.byId("slCountry").setVisible(false);
-                };
-            }
-        });
+                //    inputEmployee.setDescription("OK");
+                this.getView().byId("labelCountry").setVisible(true);
+                this.getView().byId("slCountry").setVisible(true);
+            } else {
+                // @ts-ignore
+                //    inputEmployee.setDescription("Not OK");
+                this.getView().byId("labelCountry").setVisible(false);
+                this.getView().byId("slCountry").setVisible(false);
+            };
+        };
+        return Main;
     });
