1ad76f76d57a92495cb5e1f7e737a3d75a03a112
lualtabs <lualta@altasersas.com>
2021-04-18 18:08:55 +0000

    First Commit 04

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,15 +1,32 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("logaligroup.Employees.controller.MainView", {
-			onInit: function () {
+        return Controller.extend("logaligroup.Employees.controller.MainView", {
+            onInit: function () {
 
-			}
-		});
-	});
+            },
+
+            onValidate: function () {
+                var inputEmployee = this.byId("inputEmployee");
+                // @ts-ignore
+                var valueEmployee = inputEmployee.getValue();
+                if (valueEmployee.length === 6) {
+                    // @ts-ignore
+                    //    inputEmployee.setDescription("OK");
+                    this.byId("labelCountry").setVisible(true);
+                    this.byId("slCountry").setVisible(true);
+                } else {
+                    // @ts-ignore
+                    //    inputEmployee.setDescription("Not OK");
+                    this.byId("labelCountry").setVisible(false);
+                    this.byId("slCountry").setVisible(false);
+                };
+            }
+        });
+    });
